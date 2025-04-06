cb147fc05fe2be40178aa072f7a340dd1dfbf844
janaw <janaw@LAPTOP-OB9VH4BF>
2025-04-06 18:34:45 +0530

    Dummy Project pushed

---
+++ DummyProject/src/test/java/Package1/ReadTest.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import org.testng.annotations.*;
+public class ReadTest {
+
+	@Test
+	public void WriteTest()
+	{
+		System.out.println("Write test executed successfully");
+	}
+}
---
+++ DummyProject/src/test/java/Package1/WriteTest.java
@@ -0,0 +1,12 @@
+package Package1;
+
+import org.testng.annotations.*;
+
+public class WriteTest {
+
+	@Test
+	public void ReadTest()
+	{
+		System.out.println("Read test executed successfully");
+	}
+}
